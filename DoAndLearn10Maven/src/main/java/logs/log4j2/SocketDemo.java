c72bde0e01a75d31b099c3f1f159d60e69387152
zhyu_huyu <2649160284@qq.com>
2019-01-25 12:49:45 +0800

    提交 年前

--- DoAndLearn10Maven/src/main/java/logs/log4j2/SocketDemo.java
+++ DoAndLearn10Maven/src/main/java/logs/log4j2/SocketDemo.java
@@ -4,12 +4,20 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class SocketDemo {
-    static Logger logger = (Logger) LogManager.getLogger ("apiall");
-    public static void main(String[] args) {
-        logger.error("this is log4j2 remote test error");
-        logger.debug("this is log4j2 remote test debug");
-        logger.info("this is log4j2 remote test info");
-        logger.trace("this is log4j2 remote test trace");
-    }
+	static Logger logger =  LogManager.getLogger(SocketDemo.class);
 
+	public static void main(String[] args) {
+		System.out.println("start");
+		while (true) {
+			try {
+				Thread.sleep(10000);
+				logger.error("this is log4j2 remote test error");
+				logger.debug("this is log4j2 remote test debug");
+				logger.info("this is log4j2 remote test info");
+				logger.trace("this is log4j2 remote test trace");
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}	
+	}
 }
