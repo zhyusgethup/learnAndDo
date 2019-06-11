93ee42815a86fd3f247c0f8e982063052a138fbb
zhyu <2649160284@qq.com>
2019-06-11 14:51:30 +0800

    非

--- DoAndLearn10Maven/src/main/java/utils/windows/cmd/EasyRun.java
+++ DoAndLearn10Maven/src/main/java/utils/windows/cmd/EasyRun.java
@@ -11,11 +11,13 @@ import java.util.concurrent.TimeUnit;
  */
 public class EasyRun {
     public static void main(String[] args) {
-//        CMD cmd = new EasyCMD("cmd /c G: && cd G:\\GitAndSvn\\servers\\v4-server\\server && mvn clean");
-//        cmd.setTimeout(1, TimeUnit.MINUTES);
-//        cmd.run();
+        CMD cmd = new EasyCMD("cmd /c G: && cd G:\\GitAndSvn\\servers\\v4-server\\server && mvn clean");
+//        CMD cmd = new EasyCMD("cmd /c G: && cd G:\\GitAndSvn\\servers\\v4-server && mvn clean");
+        cmd.setTimeout(1, TimeUnit.MINUTES);
+        cmd.run();
         CMD cmd2 = new EasyCMD("cmd /c G: && cd G:\\GitAndSvn\\servers\\v4-server\\server && mvn package");
-        cmd2.setTimeout(1, TimeUnit.MINUTES);
+//        CMD cmd2 = new EasyCMD("cmd /c G: && cd G:\\GitAndSvn\\servers\\v4-server && mvn install");
+        cmd2.setTimeout(20, TimeUnit.MINUTES);
         cmd2.run();
     }
 }
